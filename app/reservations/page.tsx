700f2635ba1e9810135a0433af28d15c1d9f4d4a
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2023-05-03 11:19:04 +0000

    creating route for my reservations

---
+++ app/reservations/page.tsx
@@ -0,0 +1,44 @@
+import EmptyState from '../components/EmptyState';
+import ClientOnly from '../components/clientOnly';
+import ReservationsClient from './ReservationsClient';
+
+import getCurrentUser from '../actions/getCurrentUser';
+import getReservations from '../actions/getReservations';
+
+const ReservationPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties"
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
+
+export default ReservationPage;
