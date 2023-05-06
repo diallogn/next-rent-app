ad317729d24c34368115d576f5d224a3dadac7e4
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2023-05-06 12:17:22 +0000

    creating feature of filter listings

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -1,14 +1,70 @@
 'use client';
+
 import { BiSearch } from 'react-icons/bi';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+
 const Search = () => {
+  const serachModal = useSearchModal();
+
   return (
-    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
-      <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6"> Anywhere</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+    <div
+      onClick={serachModal.onOpen}
+      className="
+      border-[1px] 
+      w-full 
+      md:w-auto 
+      py-2 
+      rounded-full 
+      shadow-sm 
+      hover:shadow-md 
+      transition 
+      cursor-pointer
+      "
+    >
+      <div
+        className="
+        flex 
+        flex-row 
+        items-center 
+        justify-between
+        "
+      >
+        <div
+          className="
+          text-sm 
+          font-semibold 
+          px-6
+          "
+        >
+          {' '}
+          Anywhere
+        </div>
+        <div
+          className="
+          hidden 
+          sm:block 
+          text-sm 
+          font-semibold 
+          px-6 
+          border-x-[1px] 
+          flex-1 
+          text-center"
+        >
           Any week
         </div>
-        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center grap-3 ">
+        <div
+          className="
+          text-sm 
+          pl-6 
+          pr-2 
+          text-gray-600 
+          flex 
+          flex-row 
+          items-center 
+          grap-3 
+          "
+        >
           <div className="hidden sm:block mr-3">Add Guests</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import NavBar from './components/navbar/navbar';
 import ClientOnly from './components/clientOnly';
-import Modal from './components/modals/modal';
 import RegisterModal from './components/modals/RegisterModal';
 import RentModal from './components/modals/RentModal';
 import LoginModal from './components/modals/LoginModal';
+import SearchModal from './components/modals/SearchModal';
 
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
@@ -31,6 +31,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <RegisterModal />
           <LoginModal />
